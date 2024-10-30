9cd064a97b9295a69676e22cf6bc5920b1b7ce23
raffaele <raffaele.fontana.02@gmail.com>
2024-10-31 00:11:26 +0100

    add spawn camera and player

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup,setup)
+        .run();
+}
+
+#[derive(Component)]
+pub struct Player {}
+
+pub fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_server.load("sprites/fighters/wind_hashashin/moveset/idle/idle_1.png"),
+            ..default()
+        },
+        Player {},
+    ));
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
 }
