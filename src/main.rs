4af0b3e0531352fa5aae845b8600cfb06833f7c3
raffaele <raffaele.fontana.02@gmail.com>
2024-11-01 16:18:47 +0100

    add window's border

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 
 pub const PLAYER_SPEED: f32 = 200.0;
+pub const PLAYER_SIZE: f32 = 64.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .add_systems(Update, player_movement)
+        .add_systems(Update, confine_player_movement)
         .run();
 }
 
@@ -36,7 +38,6 @@ pub fn setup(
     });
 }
 
-
 pub fn player_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut player_query: Query<&mut Transform, With<Player>>,
@@ -63,5 +64,38 @@ pub fn player_movement(
         }
 
         transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player_trasform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let half_player_size = PLAYER_SIZE / 2.0;
+
+        let x_min = 0.0 + half_player_size;
+        let x_max = window.width() - half_player_size;
+
+        let y_min = 0.0 + half_player_size;
+        let y_max = window.height() - half_player_size;
+
+        let mut translation = player_trasform.translation;
+
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        player_trasform.translation = translation;
     }
 }
